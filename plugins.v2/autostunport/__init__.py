947e22e688686c91096b1feefc05ca68ba0c0fe7
happyhaha1 <474248257@qq.com>
2024-11-13 21:22:44 +0800

    fix作者

--- plugins.v2/autostunport/__init__.py
+++ plugins.v2/autostunport/__init__.py
@@ -21,7 +21,7 @@ class AutoStunPort(_PluginBase):
     # 插件版本
     plugin_version = "1.0"
     # 插件作者
-    plugin_author = "jxxghp"
+    plugin_author = "happyhaha"
     # 作者主页
     author_url = "https://github.com/happyhaha1"
     # 插件配置项ID前缀
